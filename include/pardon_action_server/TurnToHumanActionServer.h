fe4a260019e8f0d2837442ade6718a2863fb6cbc
kubasikora <283418@pw.edu.pl>
2021-06-01 12:39:39 +0200

    added joy_priority_action client to switch from key_vel to joy_vel

--- include/pardon_action_server/TurnToHumanActionServer.h
+++ include/pardon_action_server/TurnToHumanActionServer.h
@@ -2,8 +2,12 @@
 #define __TURN_TO_HUMAN_ACTION_SERVER_H__
 
 #include<actionlib/server/simple_action_server.h>
+#include<actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
 #include<pardon_action_server/TurnToHumanAction.h>
+#include<twist_mux_msgs/JoyPriorityAction.h>
 #include<std_msgs/String.h>
+#include<std_msgs/Bool.h>
 #include<geometry_msgs/Quaternion.h>
 #include<nav_msgs/Odometry.h>
 #include<sensor_msgs/JointState.h>
@@ -13,10 +17,12 @@ class TurnToHumanActionServer {
   protected:
     ros::NodeHandle nh_;
 
-    actionlib::SimpleActionServer<pardon_action_server::TurnToHumanAction> as_;
     std::string actionName_;
+    actionlib::SimpleActionServer<pardon_action_server::TurnToHumanAction> as_;
     pardon_action_server::TurnToHumanFeedback feedback_;
     pardon_action_server::TurnToHumanResult result_;
+
+    actionlib::SimpleActionClient<twist_mux_msgs::JoyPriorityAction> ac_;
 
     ros::Subscriber odometrySub_;
     nav_msgs::Odometry currentOdom_;
@@ -24,15 +30,30 @@ class TurnToHumanActionServer {
     ros::Subscriber jointStateSub_;
     sensor_msgs::JointState currentJointState_;
     
+    ros::Subscriber joyPrioritySub_;
+    std_msgs::Bool currentJoyPriority_;
+
     ros::Publisher velocityPublisher_;
 
     void robotOdometryCallback(const nav_msgs::Odometry message);
     void robotJointStateCallback(const sensor_msgs::JointState message);
+    void joyPriorityCallback(const std_msgs::Bool message);
+
+    void publishFeedback(const std::string state, const geometry_msgs::Quaternion orientation);
+    void publishTorsoVelocityCommand(const double angularVelocity);
+
+    void callJoyPriorityAction();
 
   public:
-    TurnToHumanActionServer(std::string name, std::string odometryTopic = "mobile_base_controller/odom", std::string jointStateTopic = "joint_states", std::string velocityTopic = "key_vel");
+    TurnToHumanActionServer(std::string name);
     ~TurnToHumanActionServer();
     void executeCallback(const pardon_action_server::TurnToHumanGoalConstPtr &goal);                
+
+    const std::string odometryTopic = "mobile_base_controller/odom";
+    const std::string jointStateTopic = "joint_states";
+    const std::string joyPriorityTopic = "joy_priority";
+    const std::string joyPriorityAction = "joy_priority_action";
+    const std::string velocityTopic = "joy_vel";
 };
 
 #endif
--- src/TurnToHumanActionServer.cxx
+++ src/TurnToHumanActionServer.cxx
@@ -9,13 +9,45 @@ void TurnToHumanActionServer::robotJointStateCallback(const sensor_msgs::JointSt
     currentJointState_ = message;
 }
 
-TurnToHumanActionServer::TurnToHumanActionServer(std::string name, std::string odometryTopic, std::string jointStateTopic, std::string velocityTopic) : 
+void TurnToHumanActionServer::joyPriorityCallback(const std_msgs::Bool message){
+    currentJoyPriority_ = message;
+    ROS_INFO("Current joy priority equals %s", currentJoyPriority_.data ? "true" : "false");
+}
+
+void TurnToHumanActionServer::publishFeedback(const std::string state, const geometry_msgs::Quaternion orientation){
+    std_msgs::String feedbackStatus;
+    feedbackStatus.data = state;
+    feedback_.status = feedbackStatus;
+    feedback_.orientation = orientation;
+    as_.publishFeedback(feedback_);
+}
+
+void TurnToHumanActionServer::publishTorsoVelocityCommand(const double angularVelocity){
+    geometry_msgs::Twist newVelocity;
+    newVelocity.angular.z = angularVelocity;
+    velocityPublisher_.publish(newVelocity);
+}
+
+void TurnToHumanActionServer::callJoyPriorityAction(){
+    twist_mux_msgs::JoyPriorityGoal goal;
+    ac_.sendGoal(goal);
+}
+
+TurnToHumanActionServer::TurnToHumanActionServer(std::string name) : 
                       as_(nh_, name, boost::bind(&TurnToHumanActionServer::executeCallback, this, _1), false), 
+                      ac_("joy_priority_action", true),
                       actionName_(name){
-    as_.start();
     odometrySub_ = nh_.subscribe(odometryTopic, 1000, &TurnToHumanActionServer::robotOdometryCallback, this);
     jointStateSub_ = nh_.subscribe(jointStateTopic, 1000, &TurnToHumanActionServer::robotJointStateCallback, this);
+    joyPrioritySub_ = nh_.subscribe(joyPriorityTopic, 1000, &TurnToHumanActionServer::joyPriorityCallback, this);
+    
     velocityPublisher_ = nh_.advertise<geometry_msgs::Twist>(velocityTopic, 1000);
+    
+    as_.start();
+    ROS_INFO("%s server ready", name.c_str());
+
+    ac_.waitForServer();
+    ROS_INFO("%s client ready", joyPriorityAction.c_str());
 }
 
 TurnToHumanActionServer::~TurnToHumanActionServer(){}
@@ -25,6 +57,10 @@ void TurnToHumanActionServer::executeCallback(const pardon_action_server::TurnTo
     ros::Rate r(30);
     bool success = true;
 
+    bool lockedState = currentJoyPriority_.data;
+    if(!lockedState)
+        callJoyPriorityAction();
+
     for(auto i = 0; i <= 50; i++){
         if(as_.isPreemptRequested() || !ros::ok()){
             ROS_INFO("%s: Preempted", actionName_.c_str());
@@ -33,21 +69,16 @@ void TurnToHumanActionServer::executeCallback(const pardon_action_server::TurnTo
             break;
         }
 
-        std_msgs::String feedbackStatus;
-        feedbackStatus.data = "moving";
-        feedback_.status = feedbackStatus;
-
         geometry_msgs::Quaternion feedbackOrientation;
         feedbackOrientation = currentOdom_.pose.pose.orientation;
-        feedback_.orientation = feedbackOrientation;
 
-        geometry_msgs::Twist newVelocity;
-        newVelocity.angular.z = 0.5;
-        velocityPublisher_.publish(newVelocity);
-
-        as_.publishFeedback(feedback_);
+        publishTorsoVelocityCommand(0.5);
+        publishFeedback("moving", feedbackOrientation);
         r.sleep();
     }
+
+    if(!lockedState)
+        callJoyPriorityAction();
 
     if(success){
         std_msgs::String resultStatus;
